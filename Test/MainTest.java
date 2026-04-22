0ed85bc272d527d95c9c13c5d2faec9e88230e5c
Jayna Shirlie <jaynashirlie@gmail.com>
2026-04-22 09:54:28 +0530

    UC6: Addition of Two Length Units (Same Category)

--- Test/MainTest.java
+++ Test/MainTest.java
@@ -4,87 +4,98 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void testEquality_YardToYard_SameValue() {
-        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        Main.Length l2 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        assertTrue(l1.equals(l2));
+    void testAddition_FeetAndInches_ResultFeet() {
+        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.FEET);
+        Main.Length l2 = new Main.Length(12.0, Main.Length.LengthUnit.INCHES);
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.FEET);
+        assertEquals(2.0, result.getValue());
     }
 
     @Test
-    void testEquality_YardToYard_DifferentValue() {
-        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        Main.Length l2 = new Main.Length(2.0, Main.Length.LengthUnit.YARDS);
-        assertFalse(l1.equals(l2));
-    }
-
-    @Test
-    void testEquality_YardToFeet_EquivalentValue() {
+    void testAddition_YardAndFeet_ResultYard() {
         Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
         Main.Length l2 = new Main.Length(3.0, Main.Length.LengthUnit.FEET);
-        assertTrue(l1.equals(l2));
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.YARDS);
+        assertEquals(2.0, result.getValue());
     }
 
     @Test
-    void testEquality_FeetToYard_EquivalentValue() {
-        Main.Length l1 = new Main.Length(3.0, Main.Length.LengthUnit.FEET);
-        Main.Length l2 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        assertTrue(l1.equals(l2));
-    }
-
-    @Test
-    void testEquality_YardToInches_EquivalentValue() {
-        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        Main.Length l2 = new Main.Length(36.0, Main.Length.LengthUnit.INCHES);
-        assertTrue(l1.equals(l2));
-    }
-
-    @Test
-    void testEquality_InchesToYard_EquivalentValue() {
-        Main.Length l1 = new Main.Length(36.0, Main.Length.LengthUnit.INCHES);
-        Main.Length l2 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        assertTrue(l1.equals(l2));
-    }
-
-    @Test
-    void testEquality_YardToFeet_NonEquivalentValue() {
-        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        Main.Length l2 = new Main.Length(2.0, Main.Length.LengthUnit.FEET);
-        assertFalse(l1.equals(l2));
-    }
-
-    @Test
-    void testEquality_centimetersToInches_EquivalentValue() {
-        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.CENTIMETERS);
+    void testAddition_CmAndInches_ResultInches() {
+        Main.Length l1 = new Main.Length(2.0, Main.Length.LengthUnit.CENTIMETERS);
         Main.Length l2 = new Main.Length(0.393701, Main.Length.LengthUnit.INCHES);
-        assertTrue(l1.equals(l2));
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.INCHES);
+        assertTrue(result.getValue() > 0);
     }
 
     @Test
-    void testEquality_centimetersToFeet_NonEquivalentValue() {
-        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.CENTIMETERS);
+    void testAddition_SameUnit() {
+        Main.Length l1 = new Main.Length(2.0, Main.Length.LengthUnit.FEET);
+        Main.Length l2 = new Main.Length(3.0, Main.Length.LengthUnit.FEET);
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.FEET);
+        assertEquals(5.0, result.getValue());
+    }
+
+    @Test
+    void testAddition_SameReference() {
+        Main.Length l1 = new Main.Length(2.0, Main.Length.LengthUnit.FEET);
+        Main.Length result = l1.add(l1, Main.Length.LengthUnit.FEET);
+        assertEquals(4.0, result.getValue());
+    }
+
+    @Test
+    void testAddition_NullOther() {
+        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.FEET);
+        assertThrows(IllegalArgumentException.class, () -> {
+            l1.add(null, Main.Length.LengthUnit.FEET);
+        });
+    }
+
+    @Test
+    void testAddition_NullUnit() {
+        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.FEET);
         Main.Length l2 = new Main.Length(1.0, Main.Length.LengthUnit.FEET);
-        assertFalse(l1.equals(l2));
+        assertThrows(IllegalArgumentException.class, () -> {
+            l1.add(l2, null);
+        });
     }
 
     @Test
-    void testEquality_MultiUnit_TransitiveProperty() {
+    void testAddition_YardAndInches_ResultInches() {
+        Main.Length l1 = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
+        Main.Length l2 = new Main.Length(12.0, Main.Length.LengthUnit.INCHES);
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.INCHES);
+        assertEquals(48.0, result.getValue());
+    }
+
+    @Test
+    void testAddition_CmAndFeet_ResultFeet() {
+        Main.Length l1 = new Main.Length(30.48, Main.Length.LengthUnit.CENTIMETERS);
+        Main.Length l2 = new Main.Length(1.0, Main.Length.LengthUnit.FEET);
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.FEET);
+        assertTrue(result.getValue() > 1.0);
+    }
+
+    @Test
+    void testAddition_MultipleUnits() {
         Main.Length a = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
         Main.Length b = new Main.Length(3.0, Main.Length.LengthUnit.FEET);
-        Main.Length c = new Main.Length(36.0, Main.Length.LengthUnit.INCHES);
-        assertTrue(a.equals(b));
-        assertTrue(b.equals(c));
-        assertTrue(a.equals(c));
+        Main.Length result = a.add(b, Main.Length.LengthUnit.FEET);
+        assertEquals(6.0, result.getValue());
     }
 
     @Test
-    void testEquality_YardSameReference() {
-        Main.Length l = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        assertTrue(l.equals(l));
+    void testAddition_ZeroValue() {
+        Main.Length l1 = new Main.Length(0.0, Main.Length.LengthUnit.FEET);
+        Main.Length l2 = new Main.Length(2.0, Main.Length.LengthUnit.FEET);
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.FEET);
+        assertEquals(2.0, result.getValue());
     }
 
     @Test
-    void testEquality_YardNullComparison() {
-        Main.Length l = new Main.Length(1.0, Main.Length.LengthUnit.YARDS);
-        assertFalse(l.equals(null));
+    void testAddition_NegativeValues() {
+        Main.Length l1 = new Main.Length(-1.0, Main.Length.LengthUnit.FEET);
+        Main.Length l2 = new Main.Length(2.0, Main.Length.LengthUnit.FEET);
+        Main.Length result = l1.add(l2, Main.Length.LengthUnit.FEET);
+        assertEquals(1.0, result.getValue());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -27,37 +27,36 @@ public class Main {
             this.unit = unit;
         }
 
-        private double toBaseUnit() {
-            return value * unit.getFactor(); // convert to inches
+        public double getValue() {
+            return value;
         }
 
-        public double convertTo(LengthUnit targetUnit) {
-            if (targetUnit == null) throw new IllegalArgumentException();
+        private double toBaseUnit() {
+            return value * unit.getFactor();
+        }
 
-            double baseValue = this.toBaseUnit(); // in inches
-            return baseValue / targetUnit.getFactor();
+        public Length add(Length other, LengthUnit resultUnit) {
+            if (other == null || resultUnit == null) throw new IllegalArgumentException();
+            double sum = this.toBaseUnit() + other.toBaseUnit();
+            double result = sum / resultUnit.getFactor();
+            return new Length(result, resultUnit);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || !(obj instanceof Length)) return false;
+            if (obj == null) return false;
+            if (!(obj instanceof Length)) return false;
             Length other = (Length) obj;
             return Double.compare(this.toBaseUnit(), other.toBaseUnit()) == 0;
         }
     }
 
-    public static void demonstrateConversion(double value, Length.LengthUnit from, Length.LengthUnit to) {
-        Length length = new Length(value, from);
-        double result = length.convertTo(to);
-
-        System.out.println("Input: " + value + " " + from);
-        System.out.println("Converted to " + to + ": " + result);
-    }
-
     public static void main(String[] args) {
-        demonstrateConversion(1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
-        demonstrateConversion(1.0, Length.LengthUnit.YARDS, Length.LengthUnit.FEET);
-        demonstrateConversion(1.0, Length.LengthUnit.CENTIMETERS, Length.LengthUnit.INCHES);
+        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
+        Length result = l1.add(l2, Length.LengthUnit.FEET);
+
+        System.out.println("Result: " + result.getValue() + " FEET");
     }
 }
