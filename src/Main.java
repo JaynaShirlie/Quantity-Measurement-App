9addc6eb9b51927e3f189f723b40fc41a6f3f8d9
Jayna Shirlie <jaynashirlie@gmail.com>
2026-04-22 09:21:14 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,55 @@
 public class Main {
 
-    // Inner class to represent Feet measurement
     public static class Feet {
         private final double value;
 
-        // Constructor
         public Feet(double value) {
             this.value = value;
         }
 
         @Override
         public boolean equals(Object obj) {
-
-            // 1. Reference Check
-            if (this == obj) {
-                return true;
-            }
-
-            // 2. Null Check
-            if (obj == null) {
-                return false;
-            }
-
-            // 3. Type Check
-            if (!(obj instanceof Feet)) {
-                return false;
-            }
-
-            // 4. Value Comparison
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (!(obj instanceof Feet)) return false;
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    public static void main(String[] args) {
+    public static class Inches {
+        private final double value;
 
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (!(obj instanceof Inches)) return false;
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void demonstrateFeetEquality() {
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
-
         System.out.println("Input: 1.0 ft and 1.0 ft");
         System.out.println("Output: Equal (" + f1.equals(f2) + ")");
+    }
+
+    public static void demonstrateInchesEquality() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + i1.equals(i2) + ")");
+    }
+
+    public static void main(String[] args) {
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
