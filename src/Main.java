9cd3ca2dfd408e51c89beaf6bb82932d01cef714
Jayna Shirlie <jaynashirlie@gmail.com>
2026-04-22 09:13:13 +0530

    UC1: Feet measurement equality

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    // Inner class to represent Feet measurement
+    public static class Feet {
+        private final double value;
+
+        // Constructor
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            // 1. Reference Check
+            if (this == obj) {
+                return true;
+            }
+
+            // 2. Null Check
+            if (obj == null) {
+                return false;
+            }
+
+            // 3. Type Check
+            if (!(obj instanceof Feet)) {
+                return false;
+            }
+
+            // 4. Value Comparison
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + f1.equals(f2) + ")");
     }
 }
