4982dbee867cc0011f4ba256bf226e300c604428
Jayna Shirlie <jaynashirlie@gmail.com>
2026-04-22 09:23:28 +0530

    UC2: Test Cases

--- Test/MainTest.java
+++ Test/MainTest.java
@@ -3,46 +3,69 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    // Test: same values → should be equal
     @Test
-    void testEquality_SameValue() {
+    void testFeetEquality_SameValue() {
         Main.Feet f1 = new Main.Feet(1.0);
         Main.Feet f2 = new Main.Feet(1.0);
-
         assertTrue(f1.equals(f2));
     }
 
-    // Test: different values → should NOT be equal
     @Test
-    void testEquality_DifferentValue() {
+    void testFeetEquality_DifferentValue() {
         Main.Feet f1 = new Main.Feet(1.0);
         Main.Feet f2 = new Main.Feet(2.0);
-
         assertFalse(f1.equals(f2));
     }
 
-    // Test: comparing with null → should be false
     @Test
-    void testEquality_NullComparison() {
+    void testFeetEquality_NullComparison() {
         Main.Feet f1 = new Main.Feet(1.0);
-
         assertFalse(f1.equals(null));
     }
 
-    // Test: comparing with different class → should be false
     @Test
-    void testEquality_NonNumericInput() {
+    void testFeetEquality_DifferentClass() {
         Main.Feet f1 = new Main.Feet(1.0);
-        String str = "not a number";
-
+        String str = "invalid";
         assertFalse(f1.equals(str));
     }
 
-    // Test: same reference → should be true
     @Test
-    void testEquality_SameReference() {
+    void testFeetEquality_SameReference() {
         Main.Feet f1 = new Main.Feet(1.0);
-
         assertTrue(f1.equals(f1));
+    }
+
+    @Test
+    void testInchesEquality_SameValue() {
+        Main.Inches i1 = new Main.Inches(1.0);
+        Main.Inches i2 = new Main.Inches(1.0);
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        Main.Inches i1 = new Main.Inches(1.0);
+        Main.Inches i2 = new Main.Inches(2.0);
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+        Main.Inches i1 = new Main.Inches(1.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void testInchesEquality_DifferentClass() {
+        Main.Inches i1 = new Main.Inches(1.0);
+        Integer num = 5;
+        assertFalse(i1.equals(num));
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+        Main.Inches i1 = new Main.Inches(1.0);
+        assertTrue(i1.equals(i1));
     }
 }
